72dcf2ba7b94c46f4046fb5acded12b02f0a469a
Colten <likeyou600@gmail.com>
2021-06-29 16:37:08 +0800

    Add:Pumountain Models

---
+++ app/Models/ordertab_infor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ordertab_infor extends Model
+{
+    protected $table = 'ordertab_infor';
+    public $timestamps = false;
+    use HasFactory;
+}
---
+++ app/Models/ordertab_items.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ordertab_items extends Model
+{
+    protected $table = 'ordertab_items';
+    public $timestamps = false;
+    use HasFactory;
+}
