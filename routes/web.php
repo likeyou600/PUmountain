c46b0b2ca76904132f065ee0e69f6847ea46bf77
Colten <likeyou600@gmail.com>
2021-07-05 00:26:03 +0800

    Update:borrow、Auth route調整

--- routes/web.php
+++ routes/web.php
@@ -18,65 +18,60 @@ use App\Http\Controllers\UserAuthController;
 
 
 
-Route::view('PUmountain', 'PUmountain');
-Route::view('PUpicture', 'PUpicture');
+Route::view('PUmountain', 'PUmountain')->name('PUmountain');
+Route::view('PUpicture', 'PUpicture')->name('PUpicture');
 
 
-Route::get('register', [UserAuthController::class,'register']);
-Route::get('login', [UserAuthController::class,'login']);
-Route::get('logout', [UserAuthController::class,'logout']);
-Route::get('profile', [UserAuthController::class,'profile']);
+Route::view('register', 'PURegister')->name('register')->middleware('auth.backtoprofile');
+Route::view('login', 'PULogin')->name('login')->middleware('auth.backtoprofile');
+Route::view('profile', 'PUuserprofile')->name('profile')->middleware('auth.check');
 
 Route::prefix('auth')->name('auth.')->group(function () {
-
-Route::post('register', [UserAuthController::class,'store'])->name('register');
-Route::post('login', [UserAuthController::class,'logs'])->name('login');
-Route::post('change_nickname', [UserAuthController::class,'change_nickname']);
-Route::post('change_profilepicture', [UserAuthController::class,'change_profilepicture']);
-Route::post('change_password', [UserAuthController::class,'change_password']);
-
+    Route::post('register', [UserAuthController::class, 'store'])->name('register');
+    Route::post('login', [UserAuthController::class, 'logs'])->name('login');
+    Route::get('logout', [UserAuthController::class, 'logout'])->name('logout')->middleware('auth.check');
+    Route::post('change_nickname', [UserAuthController::class, 'change_nickname'])->name('change_nickname');
+    Route::post('change_picture', [UserAuthController::class, 'change_picture'])->name('change_picture');
+    Route::post('change_password', [UserAuthController::class, 'change_password'])->name('change_password');
 });
 
 
+Route::prefix('borrow')->middleware('auth.check')->name('borrow.')->group(function () {
+    Route::get('sendmail', [ToolController::class, 'sendmail'])->name('sendmail');
 
-Route::get('/borrow/mat',[ToolController::class,'mat']);
-Route::get('/borrow/bag',[ToolController::class,'bag']);
-Route::get('/borrow/backpack', [ToolController::class,'backpack']);
-Route::get('/borrow/burner',[ToolController::class,'burner']);
-Route::get('/borrow/camp',[ToolController::class,'camp']);
-Route::get('/borrow/other',[ToolController::class,'other']);
-Route::post('/add-to-cart', [ToolController::class,'AddToCart']);
-Route::get('/borrow/cart',[ToolController::class,'cart']);
-Route::post('/cartprocess', [ToolController::class,'cartprocess']);
-Route::get('/borrow/removecart', [ToolController::class,'removecart']);
-Route::get('/borrowrule', [ToolController::class,'borrowrule']);
-Route::get('/checksend', [ToolController::class,'checksend']);
-Route::get('/carttodb',[ToolController::class,'carttodb']);
-Route::get('/myorder',[ToolController::class,'myorder']);
-Route::get('/sendpicview/{order_id}',[ToolController::class,'sendpicview']);
-Route::post('/sendpic/{order_id}',[ToolController::class,'sendpic']);
-Route::get('/sendreturnpicview/{order_id}',[ToolController::class,'sendreturnpicview']);
-Route::post('/returndb/{order_id}',[ToolController::class,'returndb']);
+    Route::get('selectpage/{category}', [ToolController::class, 'article'])->name('article');
 
+    Route::post('addtocart', [ToolController::class, 'AddToCart'])->name('addtocart');
+    Route::get('cart', [ToolController::class, 'CartView'])->name('cart');
+    
+    Route::group(['middleware' => 'borrow.backtocart'], function () {
+        Route::post('updatecart', [ToolController::class, 'UpdateCart'])->name('updatecart');
+        Route::get('removecart', [ToolController::class, 'RemoveCart'])->name('removecart');
+        Route::view('rule', 'borrow/rule')->name('rule');
+        Route::get('checksend', [ToolController::class, 'checksend'])->name('checksend');
+        Route::get('cartstore', [ToolController::class, 'cartstore'])->name('cartstore');
+    });
 
-
-Route::get('/admin',[AdminController::class,'admin']);
-Route::get('/admin/member',[AdminController::class,'member']);
-Route::get('/admin/changetoadmin/{id}',[AdminController::class,'changetoadmin']);
-Route::get('/admin/equipment',[AdminController::class,'equipment']);
-Route::post('/admin/change_equipment',[AdminController::class,'change_equipment']);
-Route::get('/admin/addnewitem',[AdminController::class,'addnewitem']);
-Route::post('/admin/updatenewitem',[AdminController::class,'updatenewitem']);
-
-Route::get('/admin/adminreturn/{order_id}', [AdminController::class,'adminreturn']);
+    Route::get('myorder', [ToolController::class, 'myorder'])->name('myorder');
+    Route::post('sendpicview', [ToolController::class, 'sendpicview'])->name('sendpicview');
+    Route::post('sendpic', [ToolController::class, 'sendpic'])->name('sendpic');
+    Route::post('sendreturnpicview', [ToolController::class, 'sendreturnpicview'])->name('sendreturnpicview');
+    Route::post('sendreturnpic', [ToolController::class, 'sendreturnpic'])->name('sendreturnpic');
+});
 
 
-Route::get('/admin/allorder/all',[AdminController::class,'all']);
-Route::get('/admin/allorder/waitoget',[AdminController::class,'waitoget']);
-Route::get('/admin/allorder/borrowing',[AdminController::class,'borrowing']);
-Route::get('/admin/allorder/done',[AdminController::class,'done']);
+Route::get('/admin', [AdminController::class, 'admin']);
+Route::get('/admin/member', [AdminController::class, 'member']);
+Route::get('/admin/changetoadmin/{id}', [AdminController::class, 'changetoadmin']);
+Route::get('/admin/equipment', [AdminController::class, 'equipment']);
+Route::post('/admin/change_equipment', [AdminController::class, 'change_equipment']);
+Route::get('/admin/addnewitem', [AdminController::class, 'addnewitem']);
+Route::post('/admin/updatenewitem', [AdminController::class, 'updatenewitem']);
+
+Route::get('/admin/adminreturn/{order_id}', [AdminController::class, 'adminreturn']);
 
 
-
-
-
+Route::get('/admin/allorder/all', [AdminController::class, 'all']);
+Route::get('/admin/allorder/waitoget', [AdminController::class, 'waitoget']);
+Route::get('/admin/allorder/borrowing', [AdminController::class, 'borrowing']);
+Route::get('/admin/allorder/done', [AdminController::class, 'done']);
